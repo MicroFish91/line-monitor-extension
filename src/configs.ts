34b3fcf748234a89025700115e8aaee735324683
MicroFish91 <mwfisher91@gmail.com>
2022-04-23 14:41:50 -0700

    Rename config retrieving function

--- src/configs.ts
+++ src/configs.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 
-export async function getLineLength() {
+export async function getConfigs() {
   const configs: vscode.WorkspaceConfiguration = await vscode.workspace.getConfiguration();
   const lineLength: number | undefined = configs.get("lineMonitor.charLimit");
   return lineLength ? lineLength : 100;
--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,10 @@
 import * as vscode from "vscode";
-import { getLineLength } from "./configs";
+import { getConfigs } from "./configs";
 import { initEventListeners } from "./events";
 import { initHovers } from "./hovers";
 
 export async function activate(_context: vscode.ExtensionContext) {
-  const charLimit: number = await getLineLength();
+  const charLimit: number = await getConfigs();
   initHovers();
   initEventListeners(charLimit);
 }
